79b709574233c3528080f7da09e96a32af5ffa7a
Luke Harold Miles <lukem@sent.com>
2022-11-07 17:32:39 -0800

    boxers wiggle and shake in an ugly way

--- assets/cards/boxer.js
+++ assets/cards/boxer.js
@@ -1,19 +1,20 @@
 // Boxer
-var canvas = document.getElementById('boxer');
-var context = canvas.getContext('2d');
+const canvas = document.getElementById('boxer');
+const context = canvas.getContext('2d');
 
 canvas.width = 300;
 canvas.height = 500;
 
-var width = canvas.width;
-var height = canvas.height;
+const width = canvas.width;
+const height = canvas.height;
 
 boxer();
 
 function boxer() {
+
   // Colors
-  var yellow = '#FEAC00';
-  var colors = [
+  const yellow = '#FEAC00';
+  const colors = [
     '#FEAC00',
     '#FF5630',
     '#396C7D',
@@ -26,35 +27,72 @@ function boxer() {
     '#CF3B45',
   ];
 
+  const widths = [];
+  for (let w = 0; w < width;) {
+    widths.push(rangeFloor(10, 50));
+    w = w + widths.at(-1);
+  }
+
+  const heights = [];
+  for (const _ of widths) {
+    const arr = [];
+    for (let h = 0; h < height;) {
+      arr.push(rangeFloor(10, 50));
+      h = h + arr.at(-1);
+    }
+    heights.push(arr);
+  }
+  const hasGradient = heights.map((arr) => arr.map(() => Math.random() > 0.7));
+  const colorArrs = hasGradient.map((arr) => arr.map(has => has ? [pick(colors), pick(colors)] : pick(colors)));
+
   // Backgrounds
   context.fillStyle = yellow;
   context.fillRect(0, 0, width, height);
-
   // Double loop to draw boxes
-  for (let i = 0; i < width; ) {
-    let boxWidth = rangeFloor(10, 50);
 
-    for (let j = 0; j < height; ) {
-      let boxHeight = rangeFloor(10, 100);
+  window.count = 0;
+  function draw() {
+    count++;
+    let w = 0;
+    for (let i = 0; i < widths.length; i++) {
+      const boxWidth = widths[i + 1];
+      let h = 0;
+      for (let j = 0; j < heights[i].length; j++) {
+        let boxHeight = heights[i][j];
 
-      // Randomly add gradients
-      if (Math.random() > 0.7) {
-        let grd = context.createLinearGradient(i, j, i, j + boxHeight);
-        grd.addColorStop(0, pick(colors));
-        grd.addColorStop(1, pick(colors));
-        context.fillStyle = grd;
-      } else {
-        context.fillStyle = pick(colors);
+        // Randomly add gradients
+        if (hasGradient[i][j]) {
+          const [c1, c2] = colorArrs[i][j];
+          let grd = context.createLinearGradient(w, h, w, h + boxHeight);
+          grd.addColorStop(0, c1);
+          grd.addColorStop(1, c2);
+          context.fillStyle = grd;
+        } else {
+          context.fillStyle = colorArrs[i][j];
+        }
+
+        // Draw the boxes
+        context.fillRect(w, h, boxWidth, boxHeight);
+
+        h = h + boxHeight;
       }
-
-      // Draw the boxes
-      context.fillRect(i, j, boxWidth, boxHeight);
-
-      j = j + boxHeight;
+      w += boxWidth;
     }
-    i = i + boxWidth;
   }
-
+  function update() {
+    for (let i = 0; i < widths.length; i++) {
+      widths[i] *= (1 + Math.random() * .1 - .05);
+      for (let j = 0; j < heights[i].length; j++) {
+        heights[i][j] *= (1 + Math.random() * .1 - .05);
+      }
+    }
+  }
+  function frame() {
+    update();
+    draw();
+    requestAnimationFrame(frame);
+  }
+  frame();
   function rangeFloor(min, max) {
     // Return a random whole number between min and max
     return Math.floor(Math.random() * (max - min) + min);
